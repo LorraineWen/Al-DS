22ece983a5855dc81c4a9c8c0ce35ea1ea91c654
Lorraine Wen <2145901423@qq.com>
2024-11-15 10:01:21 +0800

    implement insert and chocie sort

--- sortalgorithm.cpp
+++ sortalgorithm.cpp
@@ -18,6 +18,39 @@ void bubblesort(int arr[], int n)
         finish=false;
     }
 }
+void choicesort(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int min_element=arr[i];
+        int min_index=i;
+        for(int j=i;j<n;j++)//用于找到数组中第i个元素到第n个元素间的最小值下标
+        {
+            if(arr[j]<min_element)
+            {
+                min_index=j;
+                min_element=arr[j];
+            }
+        }
+        swap(arr[i],arr[min_index]);//将队首元素i与最小值元素交换
+    }
+}
+void insertsort(int arr[],int n)
+{
+    //外层循环从第二个元素开始
+    for(int i=1;i<n;i++)
+    {
+        int val=arr[i];
+        int j=i-1;
+        //内层循环用于在当前元素的前面的有序序列中找到小于等于当前元素的元素，让后将当前元素插入到这个元素的后面
+        for(;j>=0;j--)
+        {
+            arr[j+1]=arr[j];
+            if(arr[j]<=val)break;
+        }
+        arr[j+1]=val;
+    }
+}
 int main()
 {
     srand(time(NULL));
@@ -33,7 +66,7 @@ int main()
         cout<<it<<" ";
     }
     cout<<endl;
-    bubblesort(arr,10);
+    choicesort(arr,10);
     for(auto it:arr)
     {
         cout<<it<<" ";
